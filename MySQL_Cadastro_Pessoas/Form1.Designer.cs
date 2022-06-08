90a466041eae6d1b33d3b9bd490a8052d80af1e8
MarcioEudes <marcioeudespf@gmail.com>
2022-06-08 11:29:47 -0300

    Exercicio_MySql_Cadastro

---
+++ MySQL_Cadastro_Pessoas/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿
+namespace MySQL_Cadastro_Pessoas
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNascimento = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.bntCadastrar = new System.Windows.Forms.Button();
+            this.bntLimpar = new System.Windows.Forms.Button();
+            this.txtSexo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.txtNacionalidade = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(7, 29);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(155, 23);
+            this.txtNome.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nome";
+            // 
+            // txtNascimento
+            // 
+            this.txtNascimento.Location = new System.Drawing.Point(8, 73);
+            this.txtNascimento.Name = "txtNascimento";
+            this.txtNascimento.Size = new System.Drawing.Size(154, 23);
+            this.txtNascimento.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nascimento";
+            // 
+            // bntCadastrar
+            // 
+            this.bntCadastrar.Location = new System.Drawing.Point(5, 287);
+            this.bntCadastrar.Name = "bntCadastrar";
+            this.bntCadastrar.Size = new System.Drawing.Size(75, 23);
+            this.bntCadastrar.TabIndex = 4;
+            this.bntCadastrar.Text = "Cadastrar";
+            this.bntCadastrar.UseVisualStyleBackColor = true;
+            this.bntCadastrar.Click += new System.EventHandler(this.bntCadastrar_Click);
+            // 
+            // bntLimpar
+            // 
+            this.bntLimpar.Location = new System.Drawing.Point(87, 287);
+            this.bntLimpar.Name = "bntLimpar";
+            this.bntLimpar.Size = new System.Drawing.Size(75, 23);
+            this.bntLimpar.TabIndex = 5;
+            this.bntLimpar.Text = "Limpar";
+            this.bntLimpar.UseVisualStyleBackColor = true;
+            this.bntLimpar.Click += new System.EventHandler(this.bntLimpar_Click);
+            // 
+            // txtSexo
+            // 
+            this.txtSexo.Location = new System.Drawing.Point(7, 117);
+            this.txtSexo.Name = "txtSexo";
+            this.txtSexo.Size = new System.Drawing.Size(155, 23);
+            this.txtSexo.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Sexo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Peso";
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(7, 165);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(155, 23);
+            this.txtPeso.TabIndex = 9;
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(7, 209);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(155, 23);
+            this.txtAltura.TabIndex = 10;
+            // 
+            // txtNacionalidade
+            // 
+            this.txtNacionalidade.Location = new System.Drawing.Point(7, 253);
+            this.txtNacionalidade.Name = "txtNacionalidade";
+            this.txtNacionalidade.Size = new System.Drawing.Size(155, 23);
+            this.txtNacionalidade.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 191);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 15);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Altura";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(9, 235);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 15);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Nacionalidade";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(174, 322);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtNacionalidade);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSexo);
+            this.Controls.Add(this.bntLimpar);
+            this.Controls.Add(this.bntCadastrar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNascimento);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNascimento;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button bntCadastrar;
+        private System.Windows.Forms.Button bntLimpar;
+        private System.Windows.Forms.TextBox txtSexo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.TextBox txtNacionalidade;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ MySQL_Cadastro_Pessoas/Form1.cs
@@ -0,0 +1,60 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MySQL_Cadastro_Pessoas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void bntCadastrar_Click(object sender, EventArgs e)
+        {
+            DateTime nascimento = Convert.ToDateTime(txtNascimento.Text);
+
+            string conexao = "Server=127.0.0.1;Database=cadastro;Uid=root;Pwd=root;";
+
+            string Insert = $@"
+            insert into pessoas(nome, nascimento, sexo, peso, altura, nacionalidade)
+            values
+            ('{ txtNome.Text}', '{nascimento.ToString("yyyy-MM-dd")}', '{txtSexo.Text}', '{txtPeso.Text}', '{txtAltura.Text}', '{txtNacionalidade.Text}')";
+
+            MySqlConnection conexaoMySql = new MySqlConnection(conexao);
+
+            try
+            {
+                conexaoMySql.Open();
+                MySqlCommand executarComando = new MySqlCommand(Insert, conexaoMySql);
+                executarComando.ExecuteNonQuery();
+
+                MessageBox.Show($"Cadastro Efetuado Com Sucesso", "Sucesso", MessageBoxButtons.OK , MessageBoxIcon.Information);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}", "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                conexaoMySql.Close();
+            }
+        }
+        private void bntLimpar_Click(object sender, EventArgs e)
+        {
+            txtNome.Text = string.Empty;
+            txtNascimento.Text = string.Empty;
+            txtSexo.Text = string.Empty;
+            txtPeso.Text = string.Empty;
+            txtAltura.Text = string.Empty;
+            txtNacionalidade.Text = string.Empty;
+        }
+    }
+}
